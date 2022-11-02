604d1a26b6ff51fa852601eef2b699ad02d6078c
jacob.ljungstrom03@gmail.com <jacob.ljungstrom03@gmail.com>
2022-11-02 23:48:50 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -8,10 +8,27 @@ class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    self.num_entry = ""
 
     # Any code you write here will run before the form opens.
 
-  def button_3_show(self, **event_args):
-    """This method is called when the Button is shown on the screen"""
-    pass
+
+  def click_3(self, **event_args):
+    self.num_entry = self.num_entry + "3"
+    if int(self.num_entry) > 51:
+      self.num_entry_color = ['#ff0000']
+    else:
+      self.num_entry_color = ['#000000']
+
+  
+    
+
+
+
+
+
+
+
+
+
 
