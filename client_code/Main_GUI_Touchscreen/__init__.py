7ef7a6f84986f15ce967f8cee418eb560ab1960f
Jacob <jacob.ljungstrom03@gmail.com>
2022-11-12 21:42:03 +0100

    syntax fix

--- client_code/Main_GUI_Touchscreen/__init__.py
+++ client_code/Main_GUI_Touchscreen/__init__.py
@@ -124,7 +124,7 @@ class Main_GUI_Touchscreen(Main_GUI_TouchscreenTemplate):
         del local_storage['unsaved']
         self.first_onlinecall_made = True
         self.onlineUpdate()
-      finally
+      finally:
         for n, value in enumerate(self.trips_td_list[:-1]):
           ##loop through to see if entries for two days are in list, checks if a more recent hour value is lesser than the previous one
           if int(self.trips_td_list[n][0:2]) < int(self.trips_td_list[n + 1][0:2]):
